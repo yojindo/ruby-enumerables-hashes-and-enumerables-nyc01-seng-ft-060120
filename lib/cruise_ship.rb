f589fd1ec0b996f036167a96c8acc49821576a6b
Jake Kwak <jakejaekwak@gmail.com>
2020-05-14 16:07:50 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.each do |key, value|
-    puts "#{key}: #{value}"
+  passengers.each do |value|
+    puts "#{value}"
   end
 end
